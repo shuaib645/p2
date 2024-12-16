aa7fb081d88dc01381ebd9e60daafc9d6d8c6d9e
shuaib645 <msamohdshuaib@gmail.com>
2024-12-16 15:35:09 +0530

    first commit

--- firebase-config.js
+++ firebase-config.js
@@ -5,12 +5,13 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-s
 
 // Your Firebase configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyCj98gerP4l4ztKfG1rPo5UzK9dgKFjlII",
+  authDomain: "myprojectp1-70080.firebaseapp.com",
+  databaseURL: "https://myprojectp1-70080-default-rtdb.firebaseio.com",
+  projectId: "myprojectp1-70080",
+  storageBucket: "myprojectp1-70080.firebasestorage.app",
+  messagingSenderId: "711696264195",
+  appId: "1:711696264195:web:cfe0a6f7a0cf1e6f4ef236"
 };
 
 // Initialize Firebase App
